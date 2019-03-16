415263e5272e9d50e0e74eecfc6bb4529aa0dac6
Dmitriy Stepanov <dima-stepan@yandex.ru>
2019-03-17 00:02:07 +0300

    fix error message

--- Shared/Engine.Core/Verify.cs
+++ Shared/Engine.Core/Verify.cs
@@ -8,14 +8,14 @@ namespace Engine.Core
             where T: IComparable<T>
         {
             if (value.CompareTo(lowerBound) < 0 || value.CompareTo(upperBound) > 0)
-                throw new ArgumentOutOfRangeException(paramName, $"Value {value} is out of range[{lowerBound}; {upperBound}]");
+                throw new ArgumentOutOfRangeException(paramName, $"Value {value} is out of range [{lowerBound}; {upperBound}]");
         }
 
         public static void InRangeExclude<T>(T value, T lowerBound, T upperBound, string paramName)
             where T : IComparable<T>
         {
             if (value.CompareTo(lowerBound) <= 0 || value.CompareTo(upperBound) >= 0)
-                throw new ArgumentOutOfRangeException(paramName, $"Value {value} is out of range({lowerBound}; {upperBound})");
+                throw new ArgumentOutOfRangeException(paramName, $"Value {value} is out of range ({lowerBound}; {upperBound})");
         }
 
         public static void IsNotNull<T>(T value, string paramName) where T: class
