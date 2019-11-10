9d06274837d4c371d179356b32fddbfa845bf1e1
Dmitrii <dima-stepan@yandex.ru>
2019-11-10 19:04:24 +0300

    ConcurrentStack added

---
+++ Engine.Core.UnitTests/ConcurrentStackTests.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using Engine.Core.Concurrent;
+using NUnit.Framework;
+
+namespace Engine.Core.UnitTests
+{
+    public sealed class ConcurrentStackTests
+    {
+        private class Stub
+        {
+
+        }
+
+        [Test]
+        public void ReturnsNullWhenEmpty()
+        {
+            var stack = new ConcurrentStack<String>();
+            Assert.IsNull(stack.Pop());
+            Assert.IsNull(stack.Pop());
+            Assert.IsNull(stack.Pop());
+        }
+
+        [Test]
+        public void ReturnsCorrectValues()
+        {
+            Stub first = new Stub();
+            Stub second = new Stub();
+            Stub third = new Stub();
+
+            var stack = new ConcurrentStack<Stub>();
+            stack.Push(first);
+            Assert.AreEqual(first, stack.Pop());
+            Assert.AreEqual(null, stack.Pop());
+
+            stack.Push(first);
+            stack.Push(second);
+            Assert.AreEqual(second, stack.Pop());
+            Assert.AreEqual(first, stack.Pop());
+            Assert.AreEqual(null, stack.Pop());
+
+            stack.Push(first);
+            stack.Push(second);
+            stack.Push(third);
+            Assert.AreEqual(third, stack.Pop());
+            Assert.AreEqual(second, stack.Pop());
+            Assert.AreEqual(first, stack.Pop());
+            Assert.AreEqual(null, stack.Pop());
+        }
+
+        [Test]
+        public void ThrowsExceptionOnNullValue()
+        {
+            var stack = new ConcurrentStack<Stub>();
+            Assert.Throws<ArgumentNullException>(() => stack.Push(null));
+        }
+    }
+}
---
+++ Engine.Core/Concurrent/ConcurrentStack.cs
@@ -0,0 +1,50 @@
+﻿using System.Threading;
+
+namespace Engine.Core.Concurrent
+{
+    public sealed class ConcurrentStack<T> where T: class
+    {
+        private volatile Node _head;
+
+        public void Push(T value)
+        {
+            Verify.IsNotNull(value, nameof(value));
+
+            var node = new Node(value);
+            Node head;
+            do
+            {
+                head = _head;
+                node._next = head;
+            } while (Interlocked.CompareExchange(ref _head, node, head) != head);
+        }
+
+        public T Pop()
+        {
+            Node next;
+            Node head;
+            do
+            {
+                head = _head;
+                if(_head == null)
+                {
+                    return null;
+                }
+                next = head._next;
+
+            } while (Interlocked.CompareExchange(ref _head, next, head) != head);
+            return head._value;
+        }
+
+        private sealed class Node
+        {
+            internal readonly T _value;
+            internal Node _next;
+
+            public Node(T value)
+            {
+                _value = value;
+            }
+        }
+    }
+}
